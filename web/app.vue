f10b85057e0dabcffd6c0be1722ef7c255b97c4c
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-06-04 02:33:26 +0430

    src and some edis

--- web/app.vue
+++ web/app.vue
@@ -13,10 +13,12 @@
   font-family: Inter
   font-weight: 400
   src: url("./assets/fonts/Inter-Regular.ttf")
+  font-display: swap
 
 @font-face
   font-family: Inter
   font-weight: 500
   src: url("./assets/fonts/Inter-Medium.ttf")
+  font-display: swap
 </style>
 <style src="./assets/sass/public/style.sass"></style>
--- web/components/layout/Header.vue
+++ web/components/layout/Header.vue
@@ -10,7 +10,7 @@ const website_links = [
 <template>
   <header class="d-flex align-items-center justify-content-center">
     <div class="desktop d-flex justify-content-between align-items-center">
-      <img src="../../assets/img/brand/logo.svg" alt="Hossara.dev">
+      <img src="~/assets/img/brand/logo.svg" alt="Hossara.dev" rel="preload">
       <div class="d-flex justify-content-center align-items-center">
         <NuxtLink v-for="link in website_links" :key="link" :to="link.url" active-class="ac">
           <span>{{ link.title }}</span>
@@ -20,8 +20,8 @@ const website_links = [
     </div>
 
     <div class="mobile d-none justify-content-between align-items-center">
-      <img src="../../assets/img/brand/logo.svg" alt="Hossara.dev"/>
-      <img src="../../assets/img/icons/icon_burger_white.svg" alt="menu"/>
+      <img src="~/assets/img/brand/logo.svg" alt="Hossara.dev" rel="preload"/>
+      <img src="~/assets/img/icons/icon_burger_white.svg" alt="menu" rel="preload"/>
     </div>
   </header>
 </template>
