32d235ea879f97090ad3b2abbcdf98afb796e69c
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-06-03 15:43:43 +0430

    TEstNUxt Structure

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main>
+    <NuxtPage/>
+  </main>
 </template>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div id="home">
+    HI
+  </div>
+</template>
+
+<style scoped>
+
+</style>
