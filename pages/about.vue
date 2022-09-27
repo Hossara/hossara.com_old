14bd4907ffe38f07e4fa88c44a43719bee1258c9
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-09-27 22:24:24 +0330

    Design About Page

--- pages/about.vue
+++ pages/about.vue
@@ -8,11 +8,17 @@ useHead({ title: "About me | Hossein Araghi" })
 </script>
 
 <template>
-  <div id="about" data-page>
-    <p>IN DEVELOPMENT</p>
+  <div id="about" class="d-flex justify-content-center align-items-center" data-page>
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <p>
+        Hi, I'm <span>Hossein Araghi</span> a <span>Software Engineer</span>. I started programming professionally in <span>2016</span> with <span>c and java</span>. My speciality is <span>Java based languages & kotlin</span>. Also i can develop user interfaces with <span>Vue.js 3 & Flutter</span>. You can read more about my specialities in <span>My Experiences</span> & <span>My Projects</span> page.
+      </p>
+      <div class="d-flex justify-content-start align-items-center w-100">
+        <RoundedButtonSmall @click="$router.push('/experiences')">My Experiences</RoundedButtonSmall>
+        <RoundedButtonSmall @click="$router.push('/projects')">My projects</RoundedButtonSmall>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped src="../assets/sass/pages/about.sass" lang="sass"></style>
