5a5cfc3d1f49b242a4f0f307de578ebd9ed09cfb
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-10-05 02:03:10 +0330

    Projects Page
    FEAT:
    Update to nuxt 11
    Add global plugin
    Add loading component
    Design projects page ( nc )

---
+++ components/ProjectsCard.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="productCards col-sm-3">
+    <div class="inner">
+      <h2>CS50x Iran</h2>
+      <p class="ps_position">Developer</p>
+      <p>DEC 2022 - OCT 2024</p>
+      <p>Spring boot, kotlin, vue.js 3, Nuxt3 RC, Mongodb, Sass</p>
+
+      <div class="d-flex justify-content-center align-items-center">
+        <a target="_blank" href="mailto:hoseinaraghi84@gmail.com" class="fa fa-link"></a>
+        <a target="_blank" href="mailto:hoseinaraghi84@gmail.com" class="fa fa-github"></a>
+        <a target="_blank" href="mailto:hoseinaraghi84@gmail.com" class="fa fa-"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="../assets/sass/widget/projectsCard.sass" lang="sass" scoped></style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,18 +1,103 @@
 <script lang="ts" setup>
-import {definePageMeta} from "#imports"
+import {definePageMeta, onMounted} from "#imports"
 import {useHead} from "#head"
+import {useNuxtApp, useState} from "#app";
+import axios from "axios";
 
 // Page Settings
 definePageMeta({ layout: "surface", name: "Projects" })
 useHead({ title: "My Projects | Hossein Araghi" })
+
+const loading = useState("loading", () => true)
+const projects = useState("projects", () => [])
+
+onMounted(() => axios.get(useNuxtApp().$api("/projects"))
+    .then(value => projects.value = value.data)
+    .catch(reason => alert(reason))
+    .finally(() => loading.value = false))
+
+const fake_projects = useState("fakeProjects", () =>
+[
+  {
+    title: "CS50x Iran",
+    tech_stack: "Spring boot, kotlin, vue.js 3, Nuxt3 RC, Mongodb, Sass",
+    position: "Developer",
+    time: ["DEC 2022", "OCT 2022"],
+    links: [
+      { type: "link", url: "https://google.com" },
+      { type: "link", url: "https://google.com" },
+      { type: "github", url: "https://google.com" }
+    ]
+  },
+  {
+    title: "CS50x Iran",
+    tech_stack: "Spring boot, kotlin, vue.js 3, Nuxt3 RC, Mongodb, Sass",
+    position: "Developer",
+    time: ["DEC 2022", "OCT 2022"],
+    links: [
+      { type: "link", url: "https://google.com" },
+      { type: "link", url: "https://google.com" },
+      { type: "github", url: "https://google.com" }
+    ]
+  },
+  {
+    title: "CS50x Iran",
+    tech_stack: "Spring boot, kotlin, vue.js 3, Nuxt3 RC, Mongodb, Sass",
+    position: "Developer",
+    time: ["DEC 2022", "OCT 2022"],
+    links: [
+      { type: "link", url: "https://google.com" },
+      { type: "link", url: "https://google.com" },
+      { type: "github", url: "https://google.com" }
+    ]
+  },
+  {
+    title: "CS50x Iran",
+    tech_stack: "Spring boot, kotlin, vue.js 3, Nuxt3 RC, Mongodb, Sass",
+    position: "Developer",
+    time: ["DEC 2022", "OCT 2022"],
+    links: [
+      { type: "link", url: "https://google.com" },
+      { type: "link", url: "https://google.com" },
+      { type: "github", url: "https://google.com" }
+    ]
+  },
+  {
+    title: "CS50x Iran",
+    tech_stack: "Spring boot, kotlin, vue.js 3, Nuxt3 RC, Mongodb, Sass",
+    position: "Developer",
+    time: ["DEC 2022", "OCT 2022"],
+    links: [
+      { type: "link", url: "https://google.com" },
+      { type: "link", url: "https://google.com" },
+      { type: "github", url: "https://google.com" }
+    ]
+  },
+])
 </script>
 
 <template>
-  <div id="projects" class="d-flex justify-content-center align-items-center" data-page>
-    <p>IN DEVELOPMENT...</p>
+  <div id="projects" class="d-flex justify-content-center align-items-center text-white" data-page>
+<!--    <LoadingWidget v-if="loading"/>
+
+    <p class="mb-0" v-else-if="projects.length === 0">No Projects Found!</p>
+
+    <p v-else>hiiiia</p>-->
+
+    <div class="row m-0">
+      <ProjectsCard v-for="project in fake_projects" :key="projects"/>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="sass" scoped>
+#projects
+  height: 100%
+  overflow: hidden
 
+  > div
+    height: 100%
+    max-width: 1600px
+    border-radius: 15px
+    overflow-y: scroll
 </style>
